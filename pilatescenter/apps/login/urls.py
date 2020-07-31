8a6d3279128c41f27e267293c61c08ba662ad360
Abdias Labrador desde ubuntu <abdiaslabrador@gmail.com>
2020-07-31 19:34:08 -0400

    modificacion de url de login

--- pilatescenter/apps/login/urls.py
+++ pilatescenter/apps/login/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name='admin_login'
 
 urlpatterns = [
-    path('login_admin', views.AdminLoginView.as_view(), name='login_admin'),
+    path('login_admin/', views.AdminLoginView.as_view(), name='login_admin'),
     path('logout_admin/', views.AdminLogoutView.as_view(), name='logout_admin')
 ]
