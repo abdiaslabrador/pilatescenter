80354fa82a3a5bfdece547531452d77124d88b5a
Abdias Labrador LAptop windows10 asus <abdiaslabrador@gmail.com>
2021-01-04 23:51:19 -0400

    modificacion de urls.py y linebreak de contact

--- pilatescenter/apps/system/urls.py
+++ pilatescenter/apps/system/urls.py
@@ -5,5 +5,5 @@ app_name='systempilates'
 
 urlpatterns = [
 	path('system/', views.SystemConfigurationView.as_view(), name='main_system'),
-	path('system/contact', views.SystemContactView.as_view(), name='system_contact'),
+	path('system/contact/', views.SystemContactView.as_view(), name='system_contact'),
 ]	
